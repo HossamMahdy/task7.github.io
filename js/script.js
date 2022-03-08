9a990f693b869e7922dc542773fb344d546fcd01
HossamMahdy <89151421+HossamMahdy@users.noreply.github.com>
2022-03-09 00:04:16 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+document.querySelector('.fa-bars').addEventListener('click',()=>(
+    document.querySelector(".nav_item").classList.toggle("active") 
+));
+document.querySelector('#nav_list_one').addEventListener('click',()=>(
+    document.querySelector(".swap_list_one").classList.toggle("active") 
+));
+document.querySelector('#nav_list_two').addEventListener('click',()=>(
+    document.querySelector(".swap_list_two").classList.toggle("active") 
+));
+document.querySelector('#nav_list_three').addEventListener('click',()=>(
+    document.querySelector(".swap_list_three").classList.toggle("active") 
+));
+
+
+
+document.querySelector('.fa-bars').addEventListener('click',()=>(
+    document.querySelector(".nav_list_menu").classList.toggle("nav_list_menu_active") 
+));
+document.querySelector('.fa-bars').addEventListener('click',()=>(
+    document.querySelector(".login_menu").classList.toggle("active") 
+));
+document.querySelector('.fa-bars').addEventListener('click',()=>(
+    document.querySelector(".signup_menu").classList.toggle("active") 
+));
+
+
+
+
+
+
